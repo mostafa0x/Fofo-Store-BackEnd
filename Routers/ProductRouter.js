5ec0b847ff98b7bbef658f2d5118556fb1f7d476
mostafa0x <dodomido12350@gmail.com>
2025-03-15 00:37:44 -0700

    Customize 401 errors authentication

--- Routers/ProductRouter.js
+++ Routers/ProductRouter.js
@@ -3,13 +3,8 @@ const Checkdb = require("../Middlewares/CheckDB");
 const CheckToken = require("../Middlewares/CheckToken");
 const ProductControlles = require("../Controllers/ProductControllers");
 
-ProductRouter.get("/Products", Checkdb, CheckToken, ProductControlles.index);
+ProductRouter.get("/Products", Checkdb, ProductControlles.index);
 
-ProductRouter.get(
-  "/Product/:id",
-  Checkdb,
-  CheckToken,
-  ProductControlles.GetByID
-);
+ProductRouter.get("/Product/:id", Checkdb, ProductControlles.GetByID);
 
 module.exports = ProductRouter;
